56bf8486395aa2a174287e7cba9829cecaa64f4f
web <bsuarez.red@gmail.com>
2017-10-21 12:16:25 -0500

    finalizado

---
+++ src/components/contacto.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Contacto</h2>
+    <ul>
+        <li v-for="tarea in tareas">
+          {{ tarea }}
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default{
+  data(){
+    return {tareas:[
+      'Calculo',
+      'Fisica',
+      'Algebra',
+      'Calidad'
+    ]}
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>home</h2>
+    <h1>{{tareas}}</h1>
+    <input type="text" placeholder="Introducir texto" v-model="nuevaTexto">
+    <input type="button" value="Enviar tarea" v-on:click="agregarTarea">
+
+  </div>
+</template>
+
+<script>
+export default{
+data(){
+  return {
+    nuevaTexto:null,
+    tareas:null
+  }
+},
+methods:{
+    agregarTarea(){
+      this.tareas=this.nuevaTexto;
+    },
+},
+}
+</script>
+
+<style>
+</style>
